b98b524cc3ef1859e13c36f41457098d86261d3b
Shriprama Rao <shriprama@gmail.com>
2019-02-17 22:04:38 -0600

    Add files via upload

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCellCollectionViewCell.swift
+//  Flix
+//
+//  Created by Shriprama Rao on 2/15/19.
+//  Copyright © 2019 Shriprama Rao. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
